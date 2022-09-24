cf1571cfd1b86ddba6c823cb75a4d68aa78da58b
Khlil1313 <90422263+Khlil1313@users.noreply.github.com>
2022-09-24 12:34:39 +0100

    Update script.js
    changed js

--- script.js
+++ script.js
@@ -78,4 +78,44 @@ new_li3.innerText = 'Netflix';
 new_li3.setAttribute("href", 'https://www.netflix.com/gb/');
 
 element_list2.appendChild(new_li3);
+var names;
 
+function getNumberOrString(value) {
+  // Convert a string value to a number if possible
+  let number_value = Number(value);
+  if (Number.isNaN(number_value)) {
+    return value
+  } else {
+    return number_value
+  }
+}
+
+function randomInt(n) {
+  // Return a random number from in [0, n[
+  return Math.floor(Math.random()*n);
+}
+
+function randomMember(arr) {
+  // Return a random member of the array
+  return arr[randomInt(arr.length)]
+}
+
+
+names = [];
+
+
+document.getElementById('add_name').addEventListener('click', (event) => {
+  names.push(getNumberOrString(document.getElementById('name').value));
+  let element_list = document.getElementById('list');
+  let new_li = document.createElement('li');
+  new_li.innerText = getNumberOrString(document.getElementById('name').value);
+
+  element_list.appendChild(new_li);
+
+});
+
+document.getElementById('select_fc').addEventListener('click', (event) => {
+  let element_list2 = document.getElementById('list');
+  element_list2.innerText = randomMember(names);
+
+});
